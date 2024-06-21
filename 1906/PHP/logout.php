574fb9204e06e08ac8de3da31bbcfd2b36161b84
oitomato <125155607+MatheusKlein@users.noreply.github.com>
2024-06-21 08:45:56 -0300

    Delete 1906 directory

--- 1906/PHP/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["email"]);
-    unset($_SESSION["nome"]);
-    unset($_SESSION["senha"]);
-    session_destroy();
-    header("location:../index.php");
-    exit();
-?>
